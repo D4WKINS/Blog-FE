3f0a290654df25b7013531e864d4269c880ae67b
Carl Dawkins <carlanthony782@gmail.com>
2021-10-05 23:09:37 +0100

    Update

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -25,7 +25,7 @@ class Login extends Component {
            <div className="text-center">Dont have an account?<Link to="/signup"> Sign Up Here</Link></div>
             <div className="text-center my-2">or</div>
 
-            <a href="http://localhost:3001/user/googleLogin" className="d-block text-center p-2" style={{border:"solid 2px lightgrey",width:"100%"}}>Login with Google</a>
+            <a href="http://localhost:3001/users/googleLogin" className="d-block text-center p-2" style={{border:"solid 2px lightgrey",width:"100%"}}>Login with Google</a>
                
                 <h3 className="text-left mt-3">Login</h3>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
--- src/components/loginGoogle.jsx
+++ src/components/loginGoogle.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import { Redirect} from "react-router-dom"
 
 const url = ""
-
+//make separate request to receive 
 const loginGoogle = () => {
     return <Redirect to="http://localhost:3001/user/googleLogin" />;
 }
